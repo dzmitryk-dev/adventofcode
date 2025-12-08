bd07d613441833b2b1b6917757ca8b2f49cc39a5
Dzmitry_Kalianchuk <dzmitry_kalianchuk@epam.com>
2025-12-08 17:18:05 +0100

    Day 8 Part 1 solved with AI help

---
+++ 2025/src/main/kotlin/day8/Day8.kt
@@ -0,0 +1,107 @@
+package day8
+
+import com.adventofcode.utils.readInput
+import com.adventofcode.utils.runPuzzle
+import java.util.stream.IntStream
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+fun main() {
+    val input = readInput("day8.input")
+
+    runPuzzle(1) {
+        part1(input, connectionsLimit = 1000, circuitsLimit = 3)
+    }
+}
+
+data class Point(val x: Int, val y: Int, val z: Int)
+
+data class Connection(val from: Point, val to: Point, val distance: Double)
+
+fun Connection.toCircuit(): Set<Point> {
+    return setOf(from, to)
+}
+
+fun parseInput(input: List<String>): List<Point> {
+    return input.map { l ->
+        l.split(",").let { (x, y, z) ->
+            Point(x.toInt(), y.toInt(), z.toInt())
+        }
+    }
+}
+
+fun calculateDistance(p1: Point, p2: Point): Double {
+    return sqrt(
+        (p2.x - p1.x).toDouble().pow(2.0) +
+        (p2.y - p1.y).toDouble().pow(2.0) +
+        (p2.z - p1.z).toDouble().pow(2.0)
+    )
+}
+
+fun buildConnections(points: List<Point>): List<Connection> {
+    return IntStream.range(0, points.size)
+        .boxed()
+        .parallel()
+        .flatMap { i ->
+            val a = points[i]
+            points.subList(i + 1, points.size)
+                .stream()
+                .map { b ->
+                    val distance = calculateDistance(a, b)
+                    Connection(a, b, distance)
+                }
+        }
+        .sorted { a, b -> a.distance.compareTo(b.distance) }
+        .toList()
+}
+
+fun buildCircuits(
+    connections: List<Connection>,
+    limit: Int,
+): List<Set<Point>> {
+    val point2circuitId = mutableMapOf<Point, Int>()
+    val circuitId2points = mutableMapOf<Int, MutableSet<Point>>()
+
+    var circuitIdCounter = 0
+
+    for (connection in connections.subList(0, limit)) {
+        val fromId = point2circuitId[connection.from]
+        val toId = point2circuitId[connection.to]
+
+        when {
+            fromId == null && toId == null -> {
+                point2circuitId[connection.from] = circuitIdCounter
+                point2circuitId[connection.to] = circuitIdCounter
+                circuitId2points[circuitIdCounter] = mutableSetOf(connection.from, connection.to)
+                circuitIdCounter++
+            }
+            fromId != null && toId == null -> {
+                point2circuitId[connection.to] = fromId
+                circuitId2points[fromId]!!.add(connection.to)
+            }
+            fromId == null && toId != null -> {
+                point2circuitId[connection.from] = toId
+                circuitId2points[toId]!!.add(connection.from)
+            }
+            fromId != null && toId != null && fromId != toId -> {
+                val fromPoints = circuitId2points[fromId]!!
+                val toPoints = circuitId2points[toId]!!
+
+                // Merge circuits
+                fromPoints.forEach { p -> point2circuitId[p] = toId }
+                toPoints.addAll(fromPoints)
+                circuitId2points.remove(fromId)
+            }
+        }
+    }
+
+    return circuitId2points.values.toList().sortedByDescending { it.size }
+}
+
+fun part1(input: List<String>, connectionsLimit: Int, circuitsLimit: Int): Int {
+    val points = parseInput(input)
+    val connections = buildConnections(points)
+    val circuits = buildCircuits(connections, limit = connectionsLimit)
+
+    return circuits.take(circuitsLimit).map { it.size }.reduce { a, b -> a * b }
+}
---
+++ 2025/src/test/kotlin/Day8Tests.kt
@@ -0,0 +1,88 @@
+import day8.Point
+import day8.buildCircuits
+import day8.buildConnections
+import day8.parseInput
+import day8.part1
+import org.assertj.core.api.Assertions.assertThat
+import kotlin.test.Test
+
+class Day8Tests {
+
+    @Test
+    fun testParsing() {
+        val expected = listOf(
+            Point(162, 817, 812),
+            Point(57, 618, 57),
+            Point(906, 360, 560),
+            Point(592, 479, 940),
+            Point(352, 342, 300),
+            Point(466, 668, 158),
+            Point(542, 29, 236),
+            Point(431, 825, 988),
+            Point(739, 650, 466),
+            Point(52, 470, 668),
+            Point(216, 146, 977),
+            Point(819, 987, 18),
+            Point(117, 168, 530),
+            Point(805, 96, 715),
+            Point(346, 949, 466),
+            Point(970, 615, 88),
+            Point(941, 993, 340),
+            Point(862, 61, 35),
+            Point(984, 92, 344),
+            Point(425, 690, 689)
+        )
+
+        val actual = parseInput(testInput.lines())
+
+        assertThat(actual).containsAll(expected)
+    }
+
+    @Test
+    fun testBuildCircuit() {
+        val circuits = buildCircuits(
+            connections = buildConnections(
+                parseInput(testInput.lines())
+            ),
+            limit = 10
+        ).take(3)
+
+        assertThat(circuits.map { it.size }).containsExactly(5, 4, 2)
+    }
+
+    @Test
+    fun testPart1() {
+        val actual = part1(testInput.lines(), 10, 3)
+
+        assertThat(actual).isEqualTo(40)
+    }
+
+
+
+    companion object {
+
+        val testInput = """
+            162,817,812
+            57,618,57
+            906,360,560
+            592,479,940
+            352,342,300
+            466,668,158
+            542,29,236
+            431,825,988
+            739,650,466
+            52,470,668
+            216,146,977
+            819,987,18
+            117,168,530
+            805,96,715
+            346,949,466
+            970,615,88
+            941,993,340
+            862,61,35
+            984,92,344
+            425,690,689
+        """.trimIndent()
+    }
+
+}
